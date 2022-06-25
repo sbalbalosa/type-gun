951ab1d7725b49c74ae5e5528d901bca1704f801
Sharlon Balbalosa <sbalbalosa@gmail.com>
2022-06-25 09:31:09 +0800

    add api usage comments

--- src/authentication/user.ts
+++ src/authentication/user.ts
@@ -3,23 +3,23 @@ import { getGun, userCreate, userAuth } from "../helpers";
 const user = getGun().user().recall({ sessionStorage: true });
 
 export default class User {
-    username: string = '';
-    password: string = '';
     gunInstance = null;
     userNode = user;
 
     static async create(username: string, password: string) {
+        if (user.is) {
+            throw new Error('Existing authenticated user');
+        }
         const instance = new User();
-        instance.username = username;
-        instance.password = password;
         try {
-            const createResult = await userCreate(this.userNode, instance.username, instance.password);
+            const createResult = await userCreate(user, username, password);
             if (createResult === true) {
-                const loginResult = await instance.login();
+                const loginResult = await instance.login(username, password);
                 loginResult && (instance.gunInstance = user);
                 return instance;
             }
         } catch(error) {
+            // TODO: throw error here instead of catching
 
             /**
              * TODO: handle these cases
@@ -27,25 +27,33 @@ export default class User {
              * If user already exists: "User already created!"
              */
             console.error(error.message);
-            return null;
         }
+        return null;
     }
 
-    async login () {
+    async login (username: string, password: string) {
         try {
-            const result = await userAuth(this.userNode, this.username, this.password);
+            const authResult = await userAuth(this.userNode, username, password);
+            if (authResult) return true;      
         } catch(error) {
+            // TODO: throw error here instead of catching
             console.error(error.message);
-            return null;
         }
+        return null;
     }
 
     logout() {
-
+        this.userNode.leave();
     }
 
     changePassword() {
 
+    }
+
+    remove(username: string, password: string) {
+        user.delete(username, password, (ack) => {
+
+        });
     }
 
     get isAuthenticated() {
@@ -55,4 +63,6 @@ export default class User {
     get publicKey() {
 
     }
+
+    // TODO: gun.on( 'auth', ack => console.log('Authentication was successful: ', ack))
 }
