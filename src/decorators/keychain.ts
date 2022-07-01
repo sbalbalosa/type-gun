0ce0e5d105980e4a90185c6c25369814b98628c2
Sharlon Balbalosa <sbalbalosa@gmail.com>
2022-07-01 15:21:56 +0800

    chore: prototype for keychain

--- src/decorators/keychain.ts
+++ src/decorators/keychain.ts
@@ -1,82 +1,118 @@
 import { generateRandomKey, fetchUser, getSea } from "../helpers";
 import Keychain from "./lib/keychain";
 import Keys from "./lib/keys";
+import Read from "./lib/read";
 import Properties from "./lib/properties";
 import SingleQuery from './query/single';
 import { getEncrypteds } from "./encrypted";
 
 const sea = getSea();
 
+//TODO: add support to sign and verify
+
 export default function keychain(constructor: Function) {
   constructor.prototype.hasAttachedKeychain = true;
   constructor.prototype.authority = null;
   constructor.prototype.keychain = new SingleQuery(constructor.prototype, Keychain);
 
-
   constructor.prototype.grantRead = async function(property: string, pub: string) {
-    if (!(await this.isKeychainExist())) throw new Error('No keychain');
-    if (!this.authority) throw new Error('No authority');
-
-    const shareUser = await fetchUser(pub);
-    if (!shareUser) throw new Error('No user');
-
-
-   if(await this.isReadExist()) {
-        const userReadAccess = await this.read.fetchById(shareUser.pub);
-        if (userReadAccess) {
-            const propertyAccess = Properties.create(userReadAccess, property);
-            const propertyKey = await this.fetchPropertyKey(property);
-            propertyAccess.key = generateRandomKey();
-            await propertyAccess.save();
-        }
-   }
-
-
-
-
-    //check if authority is same with keychain pub
+    const readAccess = await this.fetchReadAccess(pub);
+    const authority = await this.fetchAuthority();
+    const propertyAccess = await readAccess.properties.fetchById(property);
+    const decryptedMasterKey = await this.fetchPropertyKey(property);
+    const encryptedSharedKey = await sea.encrypt(decryptedMasterKey, await sea.secret(readAccess.epub, authority));
+    propertyAccess.key = encryptedSharedKey;
+    await propertyAccess.save();
   }
 
   constructor.prototype.revokeRead = async function(property: string, pub: string) {
-
+    const readAccess = await this.fetchReadAccess(pub);
+    const propertyAccess = await readAccess.properties.fetchById(property);
+    await propertyAccess.remove();
+    
   }
 
   constructor.prototype.grantReadAll = async function(pub: string) {
-
+    const fields = getEncrypteds(constructor);
+    const savePromise = fields.map(async (field) => {
+        await this.grantRead(field, pub);
+    });
+    await Promise.all(savePromise);
+    return this;
   }
 
   constructor.prototype.revokeReadAll = async function(pub: string) {
+    const fields = getEncrypteds(constructor);
+    const removePromise = fields.map(async (field) => {
+        await this.revokeRead(field, pub);
+    });
+    await Promise.all(removePromise);
+    return this;
+  }
 
+  constructor.prototype.fetchPropertyAccess = async function(pub: string, property: string) {
+    const readAccess = await this.fetchReadAccess(pub);
+    let propertyAccess = await readAccess.properties.fetchById(property);
+    if (!propertyAccess) {
+      propertyAccess = Properties.create(readAccess, property);
+      propertyAccess.pub = user.pub;
+      readAccess.epub = user.epub;
+      await readAccess.save();
+    };
+    return readAccess;
   }
 
   constructor.prototype.fetchPropertyKey = async function(property: string) {
-    
-    if (!(await this.isKeychainExist())) throw new Error('No keychain'); 
-    const propertyNode = await keychain.keys.fetchById(property);
-    if (!propertyNode) throw new Error('No key');
+    const propertyNode = await this.fetchProperty(property);
+    const authority = await this.fetchAuthority();
     await propertyNode.sync();
-    const data = await sea.decrypt(propertyNode.key, this.authority);\
+    const data = await sea.decrypt(propertyNode.key, authority);
     return data;
   }
 
-  constructor.prototype.isReadExist = async function() {
-    if (await this.isKeychainExist()) throw new Error('No keychain');
-
-    const keychain = await this.keychain.fetch();
-    return await keychain.read.isExist();
+  constructor.prototype.fetchProperty = async function(property: string) {
+    const keychain = await this.fetchKeychain();
+    const propertyNode = await keychain.keys.fetchById(property);
+    if (!propertyNode) throw new Error('No property node');
+    return propertyNode;
   }
 
-  constructor.prototype.isKeychainExist = async function() {
-    const node = await this.keychain.fetch();
-    return !!node;
+  constructor.prototype.fetchUser = async function(pub: string) {
+    const shareUser = await fetchUser(pub);
+    if (!shareUser) throw new Error('No user found');
+    return shareUser;
+  }
+
+  constructor.prototype.fetchKeychain = async function() {
+    const keychain = await this.keychain.fetch();
+    if (!keychain) throw new Error('No keychain found');
+    return keychain;
+  }
+
+  constructor.prototype.fetchReadAccess = async function(pub: string) {
+    const keychain = await this.fetchKeychain();
+    const user = await this.fetchUser(pub);
+    let readAccess = keychain.read.fetch();
+    if (!readAccess) {
+      readAccess = Read.create(this, user.pub);
+      readAccess.pub = user.pub;
+      readAccess.epub = user.epub;
+      await readAccess.save();
+    };
+    return readAccess;
+  }
+
+  constructor.prototype.fetchAuthority = async function() {
+    if (!this.authority) throw new Error('No assigned authority');
+    return this.authority;
   }
 
   constructor.prototype.createKeychain = async function() {
-    if (!this.authority) throw new Error('No authority');
+    const authority = await this.fetchAuthority();
 
     const node = Keychain.create(this);
-    node.pub = this.authority.pub;
-    node.epub = this.authority.epub;
+    node.pub = authority.pub;
+    node.epub = authority.epub;
     await node.save();
 
     await this.generatePropertyKeys(node);
@@ -84,13 +120,13 @@ export default function keychain(constructor: Function) {
   }
 
   constructor.prototype.generatePropertyKeys = async function(keychain) {
-    if (!this.authority) throw new Error('No authority');
+    const authority = await this.fetchAuthority();
 
     const fields = getEncrypteds(constructor);
     const savePromise = fields.map(async (field) => {
         const randomKey = await generateRandomKey();
         const node = Keys.create(keychain, field);
-        node.key = await sea.encrypt(randomKey, this.authority);
+        node.key = await sea.encrypt(randomKey, authority);
         await node.save();
     });
 
