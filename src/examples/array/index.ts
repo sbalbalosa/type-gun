645d980fe47e74f2f800cb1c6415385499dbaa40
Sharlon Balbalosa <sbalbalosa@gmail.com>
2022-07-15 02:52:17 +0800

    chore: added example file

---
+++ src/examples/array/index.ts
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @array
+ * class Person {
+ *     @field
+ *    name: string;
+ * }
+ * 
+ * const person = Person.create(rootNode, index); // index is optional
+ * person.name = "John Doe";
+ * await person.save();
+ * 
+ * 
+ * 
+ */
