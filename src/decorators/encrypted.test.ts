c18bfcc6cec31a44eda29e8e79afcaa411b600df
Sharlon Balbalosa <sbalbalosa@gmail.com>
2022-07-26 18:16:54 +0800

    test: completed encrypted unit test

--- src/decorators/encrypted.test.ts
+++ src/decorators/encrypted.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
 import 'reflect-metadata';
-import encrypted, { createEncryptedData, getEncrypteds } from './encrypted';
+import encrypted, { createDecryptedData, createEncryptedData, getEncrypteds } from './encrypted';
 
 class Person {
   @encrypted
@@ -17,6 +17,8 @@ class NoField {
 }
 
 const mockEncryptionFn = vi.fn((_, value) => (`encrypted${value}`));
+const mockDecryptionFn = vi.fn((_, value) => (`decrypted${value}` as any));
+
 
 describe('encrypted', () => {
     it('should register decorated encrypted property to metadata', () => {
@@ -47,7 +49,6 @@ describe('encrypted', () => {
 
         const result = await createEncryptedData(raw, person, Person);
         expect(result).toEqual({ name: `encrypted${person.name}`, age: `encrypted${person.age}` });
-
     });
 
     it('should not encrypt data when there are no encrypted fields', async () => {
@@ -58,26 +59,97 @@ describe('encrypted', () => {
         const raw = { test: noField.test };
 
         const result = await createEncryptedData(raw, noField, NoField);
-        expect(result).toEqual({ test: 'test' });
+        expect(result).toEqual(raw);
     });
 
-    // it('should create decrypted data', async () => {
+    it('should not encrypt data when instance has no keychain', async () => {
+        const person = new Person();
+        person.name = 'David';
+        person.age = 3;
+        person.hasKeychain = false;
+        const raw = { name: person.name, age: person.age };
 
+        const result = await createEncryptedData(raw, person, Person);
+        expect(result).toEqual(raw);
+    });
 
-    // });
+    it('should create decrypted data', async () => {
+        const raw = {
+            name: 'encryptedName',
+            age: 'encryptedAge',
+        };
+        const mockFetchKeychainFn = vi.fn(() => Promise.resolve({
+            isAuthorityOwner: vi.fn(() => true),
+        }));
+        const person = new Person();
+        person.hasKeychain = true;
+        person.decryptProperty =  mockDecryptionFn;
+        person.fetchKeychain = mockFetchKeychainFn;
 
-    // it('should create a new object with values coming from another object with decorated fields', () => {
+        const result = await createDecryptedData(raw, person, Person);
+        expect(result).toEqual({ name: `decrypted${raw.name}`, age: `decrypted${raw.age}` });
+    });
 
-    //     const person = new Person();
-    //     person.name = 'David';
-    //     person.age = 3;
-    //     person.time = 'test';
+    it('should not decrypt data when instance has no keychain', async () => {
+        const raw = {
+            name: 'encryptedName',
+            age: 'encryptedAge',
+        };
+        const person = new Person();
+        person.hasKeychain = false;
+        const result = await createDecryptedData(raw, person, Person);
+        expect(result).toEqual(raw);
+    });
 
-    //     const raw = createFieldRawData(person, Person);
+    it('should not decrypt data when there are no encrypted fields', async () => {
+        const noField = new NoField();
+        noField.test = 'test';
+        noField.hasKeychain = true;
+        noField.decryptProperty =  mockDecryptionFn;
+        const raw = { test: noField.test };
 
-    //     expect(raw).toEqual({
-    //         name: 'David',
-    //         age: 3
-    //     })
-    // });
+        const result = await createDecryptedData(raw, noField, NoField);
+        expect(result).toEqual(raw);
+    });
+
+    it('should not decrypt data when the attached authority is not the owner of data', async () => {
+        const raw = {
+            name: 'encryptedName',
+            age: 'encryptedAge',
+        };
+
+        const mockFetchKeychainFn = vi.fn(() => Promise.resolve({
+            isAuthorityOwner: vi.fn(() => false), 
+        }));
+        const mockDecryptionFn = vi.fn(() => {
+            throw new Error('test');
+        });
+        const person = new Person();
+        person.hasKeychain = true;
+        person.decryptProperty =  mockDecryptionFn;
+        person.fetchKeychain = mockFetchKeychainFn;
+
+        const result = await createDecryptedData(raw, person, Person);
+        expect(result).toEqual(raw);
+    });
+
+    it('should throw an error if data could not be decrypted and attached authority is the owner', async () => {
+        const raw = {
+            name: 'encryptedName',
+            age: 'encryptedAge',
+        };
+
+        const mockFetchKeychainFn = vi.fn(() => Promise.resolve({
+            isAuthorityOwner: vi.fn(() => true), 
+        }));
+        const mockDecryptionFn = vi.fn(() => {
+            throw new Error('testError');
+        });
+        const person = new Person();
+        person.hasKeychain = true;
+        person.decryptProperty =  mockDecryptionFn;
+        person.fetchKeychain = mockFetchKeychainFn;
+
+        await expect(createDecryptedData(raw, person, Person)).rejects.toThrow('testError');
+    });
 });
